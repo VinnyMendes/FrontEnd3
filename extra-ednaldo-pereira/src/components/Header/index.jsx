805540f64d570dcaac2090aeea823ce1b442fc1d
VinnyMendes <vinypotter.vm@gmail.com>
2021-10-28 15:16:34 -0300

    aa

--- extra-ednaldo-pereira/src/components/Header/index.jsx
+++ extra-ednaldo-pereira/src/components/Header/index.jsx
@@ -6,8 +6,7 @@ export default function Header(props){
             <h1>{props.empresa}</h1>
             <nav>
                 <ul>
-                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <li><a href="#">Ednaldo</a></li>
+                    <li><a href="#ednaldo">Ednaldo</a></li>
                     <li><a href="#videos">Videos</a></li>
                     <li><a href="#footer">Footer</a></li>
                 </ul>
