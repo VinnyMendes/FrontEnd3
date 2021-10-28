cd955b60093a4cd555d81ecd044d1ebf45e7d889
VinnyMendes <vinypotter.vm@gmail.com>
2021-10-28 15:20:43 -0300

    aaa

--- extra-ednaldo-pereira/src/App.js
+++ extra-ednaldo-pereira/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
-import Ednaldo from './components/Ednaldo';
 import Videos from './components/Videos';
 import Footer from './components/Footer';
 
